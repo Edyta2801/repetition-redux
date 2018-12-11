d045902a99b53682bd2a17b224d0e397ebd56023
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-11 11:16:11 +0100

    Task 6 i 7

--- src/store.js
+++ src/store.js
@@ -1,12 +1,37 @@
-import {createStore}from 'redux'
+import { createStore } from 'redux'
 
+
+const INITIAL_STATE = {
+    firstAction: false
+}
 
 //reducer wywołuje akcję INIT
-const reducer=()=>({
-    name:'Ala'
-})
 
-export const store =createStore(
+const reducer = (state = INITIAL_STATE, action) => {
+    if (action.typ === 'FIRST_ACTION') {
+        return {
+            ...state,
+            firstAction: true
+        }
+    } return state//by default returns prev state
+}
+
+
+
+
+
+
+
+export const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+
+store.dispatch({
+    type: 'FIRST_ACTION'
+})
+
+store.dispatch({
+    type: 'FIRST_ACTION'
+})
